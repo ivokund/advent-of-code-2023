68d5b63854ce1f63a096769018496f1e12d0204c
ivokund <ivo@pactum.com>
2023-12-04 18:42:19 +0200

    Template

---
+++ _template/run.kts
@@ -0,0 +1,19 @@
+import java.io.File
+
+val testInput = """
+
+""".trimIndent().lines()
+
+val realInput = File("day__/input.txt").readLines()
+
+fun part1(lines: List<String>): Int {
+    return 0
+}
+
+println("--- test input")
+println(part1(testInput))
+// println(part2(testInput))
+
+println("--- real input")
+// println(part1(realInput))
+// println(part2(realInput))
