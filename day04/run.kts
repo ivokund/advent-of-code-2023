c33118ef421ffc81d512e8cab66817f59a67975a
ivokund <ivo@pactum.com>
2023-12-04 21:56:46 +0200

    Day 4, part 2

--- day04/run.kts
+++ day04/run.kts
@@ -13,24 +13,29 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
 val realInput = File("day04/input.txt").readLines()
 
 data class Card(val id: Int, val myNumbers: List<Int>, val winningNumbers: List<Int>) {
-    private fun getMatchingNumbers(): List<Int> {
-        return myNumbers.filter { it in winningNumbers }
+    var copies: Int = 1
+    fun getMatchCount(): Int {
+        return myNumbers.filter { it in winningNumbers }.size
     }
 
     fun getScore(): Double {
-        val count = this.getMatchingNumbers().size
+        val count = getMatchCount()
         if (count == 0) return 0.0
         return 2.0.pow(count.toDouble() - 1)
+    }
+
+    fun addCopies(num: Int) {
+        this.copies += num
     }
 }
 
 fun lineToCard(line: String): Card {
     return Regex("^Card\\s+(\\d+):\\s(.*)$").matchEntire(line)!!.let { match ->
         val (id, numbers) = match.destructured
-        val (myNumbers, winningNumbers) = numbers.split(" | ")
-        val my = Regex("\\d++").findAll(myNumbers).map { it.value.toInt() }.toList()
-        val winning = Regex("\\d++").findAll(winningNumbers).map { it.value.toInt() }.toList()
-        Card(id.toInt(), my, winning)
+        val (myNumbers, winningNumbers) = numbers.split(" | ").map {
+            Regex("\\d++").findAll(it).map { match -> match.value.toInt() }.toList()
+        }
+        Card(id.toInt(), myNumbers, winningNumbers)
     }
 }
 
@@ -38,10 +43,22 @@ fun part1(lines: List<String>): Int {
     return lines.sumOf { lineToCard(it).getScore() }.toInt()
 }
 
+fun part2(lines: List<String>): Int {
+    val cardsById = lines.map { lineToCard(it) }.associateBy { it.id }
+
+    cardsById.forEach { (_, card) ->
+        for (number in 1..card.getMatchCount()) {
+            cardsById[card.id + number]?.addCopies(card.copies)
+        }
+    }
+
+    return cardsById.values.sumOf { it.copies }
+}
+
 println("--- test input")
 println(part1(testInput))
-// println(part2(testInput))
+println(part2(testInput))
 
 println("--- real input")
 println(part1(realInput))
-// println(part2(realInput))
+println(part2(realInput))
