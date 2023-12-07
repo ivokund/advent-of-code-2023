e762c10fd65d5d305efc2acd93a8a4f3628db3da
ivokund <ivo@pactum.com>
2023-12-07 23:54:45 +0200

    Day 7, part 2 correct

--- day07/run.kts
+++ day07/run.kts
@@ -46,17 +46,16 @@ fun getJokerRating(cards: List<Char>): Rating {
         .map { (k, v) -> k to v.size }
         .associate { (k, v) -> k to v }
         .toMutableMap()
-
     do {
+        var adjusted = false
         val jokerCount = groups['J'] ?: 0
         if (jokerCount in 1..4) {
-//                println(groups)
-            val maxCard = groups.filter { it.key != 'J' && it.value < 5 }.maxBy { it.key }.key
-
+            val maxCard = groups.filter { it.key != 'J' && it.value < 5 }.maxBy { it.value }.key
             groups[maxCard] = groups[maxCard]!! + 1
             groups['J'] = groups['J']!! - 1
+            adjusted = true
         }
-    } while (jokerCount > 0)
+    } while (adjusted)
     return if (groups.values.contains(5)) {
         Rating.FIVE_OF_KIND
     } else if (groups.values.contains(4)) {
@@ -95,22 +94,24 @@ fun sortHands(hands: List<Hand>, cardRatings: List<Char>): List<Hand> {
 }
 
 fun part1(lines: List<String>): Int {
+    val cardRatings = listOf('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A')
+
     val hands = lines.map {
         val (cards, bid) = it.split(" ")
         Hand(cards.toList(), bid.toInt(), getNormalRating(cards.toList()))
     }
-    val cardRatings = listOf('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A')
     val sortedHands = sortHands(hands, cardRatings)
-
     return sortedHands.foldIndexed(0) { index, acc, hand -> (index + 1) * hand.bid + acc }
 }
 
 fun part2(lines: List<String>): Int {
+    val cardRatings = listOf('J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A')
+
     val hands = lines.map {
         val (cards, bid) = it.split(" ")
         Hand(cards.toList(), bid.toInt(), getJokerRating(cards.toList()))
     }
-    val cardRatings = listOf('J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A')
+
     val sortedHands = sortHands(hands, cardRatings)
     return sortedHands.foldIndexed(0) { index, acc, hand -> (index + 1) * hand.bid + acc }
 }
@@ -122,3 +123,6 @@ println(part2(testInput))
 println("--- real input")
 println(part1(realInput))
 println(part2(realInput))
+
+// 254164885
+// 254177478
