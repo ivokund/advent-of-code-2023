3c0af3b443ca4b3f28994e0f0de7f2ca3097251f
ivokund <ivo@pactum.com>
2023-12-15 01:30:01 +0200

    Day 13, part 2

--- day13/run.kts
+++ day13/run.kts
@@ -2,23 +2,21 @@ import java.io.File
 import kotlin.math.abs
 
 val testInput = """
-...#...####...#..
-.....##.##.##....
-##....######....#
-..#.##.#..#.##...
-##.###.####.###.#
-..###...##...###.
-#####.##..##.####
-#######....######
-###...#.##.#...##
-....###.##.###...
-##.####.##.####.#
-..###...##...###.
-##.#.##....##.#.#
-##..#.#....#.#..#
-##.###.#..#.###.#
-###.#...##...#.##
-..####.####.####.
+#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#
 """.trimIndent()
 
 val realInput = File("day13/input.txt").readText()
@@ -29,60 +27,68 @@ data class Coords(val x: Int, val y: Int) {
     }
 }
 
+enum class ReflectionType {
+    Horizontal,
+    Vertical,
+}
+
+data class ReflectionLine(val type: ReflectionType, val position: Int)
+
+fun List<ReflectionLine>.score(): Int {
+    return this.sumOf {
+        it.position * (if (it.type == ReflectionType.Horizontal) 100 else 1)
+    }
+}
+
+fun isMirroring(rows: List<String>, x: Int): Boolean {
+    if (rows[x] == rows[x + 1]) {
+        val newRows = rows.filterIndexed { index, _ -> index !in x..x + 1 }
+
+        if (x < 1 || x > newRows.size - 1) {
+            return true
+        }
+        return isMirroring(newRows, x - 1)
+    }
+    return false
+}
+
 data class Diagram(val rocks: Set<Coords>) {
     private val xMin = 0
     private val xMax = rocks.maxOf { it.x }.toInt()
     private val yMin = 0
     private val yMax = rocks.maxOf { it.y }.toInt()
 
-    fun getLinesLeftToReflection(): Int? {
-        val columns = getColumns()
-
-        fun colsAreMirroring(cols: List<String>, x: Int): Boolean {
-//            println("checking $x, size: ${cols.size}")
-            if (cols[x] == cols[x + 1]) {
-//                println(" .. match at $x: ${cols[x]} matches ${cols[x + 1]}")
-                val newCols = cols
-                    .filterIndexed { index, _ -> index !in x..x+1 }
-
-                if (x < 1 || x > newCols.size - 1) {
-                    return true
-                }
-                return colsAreMirroring(newCols, x - 1)
-            }
-            return false
-        }
-
-        for (x in 0..<xMax) {
-            if (colsAreMirroring(columns, x)) {
-                return x + 1
-            }
-        }
-        return null
+    fun getReflectionLine(not: ReflectionLine? = null): ReflectionLine? {
+        return (emptyList<ReflectionLine>() +
+                getLinesLeftToReflection().map { ReflectionLine(ReflectionType.Vertical, it) } +
+                getLinesAboveReflection().map { ReflectionLine(ReflectionType.Horizontal, it) })
+            .firstOrNull { it != not }
     }
-    fun getLinesAboveReflection(): Int? {
+
+    fun getLinesLeftToReflection(): List<Int> {
+        val columns = getColumns()
+        return (0 until xMax).filter { isMirroring(columns, it) }.map { it + 1 }
+    }
+
+    fun getLinesAboveReflection(): List<Int> {
         val rows = getRows()
+        return (0 until yMax).filter { isMirroring(rows, it) }.map { it + 1 }
+    }
 
-        fun rowsAreMirroring(rows: List<String>, y: Int): Boolean {
-//            println("checking $y, size: ${rows.size}")
-            if (rows[y] == rows[y + 1]) {
-                val newRows = rows
-                    .filterIndexed { index, _ -> index !in y..y+1 }
-
-                if (y < 1 || y > newRows.size - 1) {
-                    return true
+    fun fixSmudge(): ReflectionLine {
+        val currentLine = getReflectionLine()!!
+        for (y in yMin..yMax) {
+            for (x in xMin..xMax) {
+                val coords = Coords(x, y)
+                val newDiagram = Diagram(
+                    if (rocks.contains(coords)) rocks.minusElement(coords) else rocks.plusElement(coords)
+                )
+                newDiagram.getReflectionLine(currentLine)?.let {
+                    return it
                 }
-                return rowsAreMirroring(newRows, y - 1)
-            }
-            return false
-        }
-
-        for (y in 0..<yMax) {
-            if (rowsAreMirroring(rows, y)) {
-                return y + 1
             }
         }
-        return null
+        throw Error("Did not find a smudge")
     }
 
     fun getRows(): List<String> {
@@ -127,26 +133,20 @@ fun parseInput(lines: List<String>) = Diagram(
 
 fun part1(lines: String): Int {
     val diagrams = lines.split("\n\n").map { parseInput(it.lines()) }
+    return diagrams.map { it.getReflectionLine()!! }.score()
+}
 
-    val results = diagrams.mapIndexed { index, diagram ->
-//        println("diagram $index")
+fun part2(lines: String): Int {
+    val diagrams = lines.split("\n\n").map { parseInput(it.lines()) }
+    val smudgeFixedLines = diagrams.map { it.fixSmudge() }
+    return smudgeFixedLines.score()
 
-        val cols = diagram.getLinesLeftToReflection()
-        val rows = diagram.getLinesAboveReflection()
-
-        Pair(cols, rows)
-    }
-
-    results.forEach { println(it) }
-
-    return results.sumOf { it.first ?: 0 } + 100 * results.sumOf { it.second ?: 0 }
 }
 
 println("--- test input")
 println(part1(testInput))
-// println(part2(testInput))
+println(part2(testInput))
 
 println("--- real input")
- println(part1(realInput))
-// println(part2(realInput))
-// 42361
+println(part1(realInput))
+println(part2(realInput))
