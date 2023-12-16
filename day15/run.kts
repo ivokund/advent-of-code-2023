b2a7c88e20341599c68686c3452c7127a60a14a7
ivokund <ivo@pactum.com>
2023-12-16 18:34:00 +0200

    Day 15, part 2

--- day15/run.kts
+++ day15/run.kts
@@ -9,14 +9,47 @@ val realInput = File("day15/input.txt").readText()
 fun hash(input: String) = input.fold(0) { acc, c -> ((acc + c.code) * 17) % 256 }
 check(hash("HASH") == 52)
 
+data class Lens(val label: String, val focalLength: Int) {
+    override fun toString() = "[$label $focalLength]"
+}
+
 fun part1(input: String): Int {
     return input.split(',').sumOf { hash(it) }
 }
 
+fun part2(input: String): Int {
+    val boxes = mutableMapOf<Int, MutableList<Lens>>()
+    input.split(',').forEach {
+        val (label, operation, foo) = Regex("(\\w+)(=|\\-)(\\w*)").matchEntire(it)!!.destructured
+        val boxNo = hash(label)
+        if (boxNo !in boxes) boxes[boxNo] = mutableListOf()
+        if (operation == "=") {
+            val index = boxes[boxNo]!!.indexOfFirst { it.label == label }
+            val lens = Lens(label, foo.toInt())
+            if (index == -1) {
+                boxes[boxNo]!!.add(lens)
+            } else {
+                boxes[boxNo]!![index] = lens
+            }
+        } else {
+            boxes[boxNo]!!.removeIf { it.label == label }
+        }
+    }
+
+    val focusingPowers = boxes.flatMap {
+        val boxNo = it.key + 1
+        it.value.mapIndexed { slotNo, lens ->
+            lens.focalLength * (slotNo + 1) * boxNo
+        }
+    }
+
+    return focusingPowers.sum()
+}
+
 println("--- test input")
 println(part1(testInput))
-// println(part2(testInput))
+println(part2(testInput))
 
 println("--- real input")
 println(part1(realInput))
-// println(part2(realInput))
+println(part2(realInput))
