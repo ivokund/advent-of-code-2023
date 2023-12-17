4f19a688b9d90b76e076905b2fe6a9c11baf9743
ivokund <ivo@pactum.com>
2023-12-17 16:08:50 +0200

    Day 16, part 2

--- day16/run.kts
+++ day16/run.kts
@@ -50,20 +50,29 @@ data class Diagram(val tiles: Map<Coords, Char>) {
     fun draw() {
         println(getRows().joinToString("\n"))
     }
+
+    fun getEntryPoints(): List<Pair<Char, Coords>> { // direction + position
+        val entryPoints = mutableListOf<Pair<Char, Coords>>()
+        for (x in xMin..xMax) {
+            entryPoints.add(Pair('S', Coords(x, yMin)))
+            entryPoints.add(Pair('N', Coords(x, yMax)))
+        }
+        for (y in yMin..yMax) {
+            entryPoints.add(Pair('E', Coords(xMin, y)))
+            entryPoints.add(Pair('W', Coords(xMax, y)))
+        }
+        return entryPoints
+    }
 }
 
-
-val realInput = File("day16/input.txt").readLines()
-
-fun part1(lines: List<String>): Int {
-    val diagram = fromRows(lines)
-
+fun getEnergizedTiles(diagram: Diagram, startPos: Coords, direction: Char): Set<Coords> {
     data class StackItem(val direction: Char, val nextPos: Coords)
 
+    // set of vectors: direction + coordinate
     val visitedSet = mutableSetOf<Pair<Char, Coords>>()
     val energizedSet = mutableSetOf<Coords>()
 
-    val stack = mutableListOf(StackItem('E', Coords(0, 0)))
+    val stack = mutableListOf(StackItem(direction, startPos))
 
     val coordDeltas = mapOf(
         'E' to Coords(1, 0),
@@ -103,11 +112,7 @@ fun part1(lines: List<String>): Int {
     do {
         val stackItem = stack.removeLast()
         val at = stackItem.nextPos
-
         val tile = diagram.tiles.getOrDefault(at, '.')
-
-        println("at $at, tile $tile, direction ${stackItem.direction}")
-
         val newItems = mutableListOf<StackItem>()
 
         if (tile == '.') {
@@ -137,20 +142,31 @@ fun part1(lines: List<String>): Int {
         })
 
     } while (stack.size > 0)
+    return energizedSet
+}
 
-    diagram.draw()
 
+val realInput = File("day16/input.txt").readLines()
 
-    val energized = Diagram(energizedSet.map { it to 'O' }.toMap())
-    energized.draw()
-
+fun part1(lines: List<String>): Int {
+    val diagram = fromRows(lines)
+    val energizedSet = getEnergizedTiles(diagram, Coords(0, 0), 'E')
     return energizedSet.size
+}
+
+fun part2(lines: List<String>): Int {
+    val diagram = fromRows(lines)
+    val entryPoints = diagram.getEntryPoints()
+    val sums = entryPoints.map { (direction, startPos) ->
+        getEnergizedTiles(diagram, startPos, direction).size
+    }
+    return sums.max()
 }
 
 println("--- test input")
 println(part1(testInput))
-// println(part2(testInput))
+println(part2(testInput))
 
 println("--- real input")
 println(part1(realInput))
-// println(part2(realInput))
+println(part2(realInput))
